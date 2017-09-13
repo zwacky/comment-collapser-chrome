77122f5e581b5bc57349757c5b4a2bb9b3da727a
Simon Wicki <simon.wicki@justwatch.com>
2017-09-13 23:33:35 +0200

    fix: user can set the current target by clicking it directly

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -17,10 +17,11 @@ var expandSelector = (location.href.indexOf(sites.reddit.url) !== -1) ?
 	sites.hackernews.expandSelector;
 
 updateExpands();
+registerExpandClickEvents();
 
 
 // go to next expand button
-window.addEventListener('keydown', function(evt) {
+window.addEventListener('keydown', function (evt) {
 	// don't run shortcuts when modifiers & comment box is in focus
 	if (!hasModifierKeysActive(evt) && isCommentBoxFocused()) {
 		return;
@@ -43,8 +44,8 @@ window.addEventListener('keydown', function(evt) {
 
 function hasModifierKeysActive(evt) {
 	return ['shiftKey', 'metaKey', 'ctrlKey']
-		.map(function(key) { return evt[key]; })
-		.filter(function(value) { return value; })
+		.map(function (key) { return evt[key]; })
+		.filter(function (value) { return value; })
 		.length > 0;
 }
 
@@ -57,29 +58,28 @@ function isCommentBoxFocused() {
  * 
  * @param direction either 1, -1
  */
-function moveToExpand(direction, isMoving) {
-	if (currentExpandElement === null) {
-		currentExpandElement = expands[0];
-	}
-
+function moveToExpand(direction) {
 	// go through expands to mach currentExpandElement and then move accordingly
 	var currentExpandIndex = expands
-		.reduce(function(root, item, index) {
+		.reduce(function (root, item, index) {
 			return (item === currentExpandElement) ?
 				index :
 				root;
-		}, 0);
-	
+		}, null);
+
 	var targetExpandIndex = (direction > 0) ?
-		((currentExpandIndex !== expands.length - 1) ? currentExpandIndex + 1 : 0) :
-		((currentExpandIndex !== 0) ? currentExpandIndex - 1 : 0);
-	
+		((currentExpandIndex !== null && currentExpandIndex !== expands.length - 1) ? currentExpandIndex + 1 : 0) :
+		((currentExpandIndex !== null && currentExpandIndex !== 0) ? currentExpandIndex - 1 : 0);
+
+
 	// setting current expand element
 	currentExpandElement = expands[targetExpandIndex];
-	
+
 	// scrolling to current expand element
 	scrollTo(currentExpandElement);
 }
+
+// everytime a comment toggle happens -> set currentExpandElement
 
 function toggleComment(evt) {
 	if (currentExpandElement === null) {
@@ -89,17 +89,41 @@ function toggleComment(evt) {
 	evt.preventDefault();
 
 	currentExpandElement.click();
-
-	updateExpands();
 }
 
 function scrollTo(element) {
-	// element.scrollIntoView(true);
-	element.scrollIntoView({block: 'start', inline: 'start', behavior: 'smooth'});
+	element.scrollIntoView({ block: 'start', inline: 'start', behavior: 'smooth' });
 }
 
 function updateExpands() {
 	// check if we are on hacker news or reddit
 	expands = Array.prototype.slice.call(document.querySelectorAll(expandSelector))
-		.filter(function(el) { return el.offsetWidth !== 0 && el.offsetHeight !== 0; }); // check for non-hidden elements only
+		.filter(function (el) { return el.offsetWidth !== 0 && el.offsetHeight !== 0; }); // check for non-hidden elements only
+}
+
+/**
+ * whenever a user clicks an expand button, that expand button will be treated as the `currentExpandElemen`.
+ * since through toggling comments they can be hidden or shown, the clicks need to be removed and added at every `toggleComment`.
+ */
+function registerExpandClickEvents() {
+	expands.forEach(function (expand) {
+		expand.addEventListener('click', setCurrentExpandElementByClick);
+	});
+}
+
+/**
+ * removes the click handler from expand buttons.
+ */
+function unregisterExpandClickEvents() {
+	expands.forEach(function (expand) {
+		expand.removeEventListener('click', setCurrentExpandElementByClick)
+	});
+}
+
+function setCurrentExpandElementByClick(evt) {
+	currentExpandElement = evt.target;
+
+	unregisterExpandClickEvents();
+	updateExpands();
+	registerExpandClickEvents();
 }
